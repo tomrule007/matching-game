c3e18c72b8765fb98da8c2ecdf72d8e6e382064b
Thomas Herzog <Tomrule007@gmail.com>
2019-10-07 19:29:25 -0700

    Created TimerDisplay component.
    added simple 1second interval ticker that is started when mounted.

---
+++ src/components/TimerDisplay.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+
+const TimerDisplay = () => {
+  const [time, setTime] = useState({ seconds: 0, minutes: 0 });
+  useEffect(() => {
+    const ticker = setInterval(() => {
+      console.log('tick');
+      const seconds = time.seconds + 1;
+      setTime(
+        seconds > 60
+          ? { minutes: time.minutes + 1, seconds: 0 }
+          : { minutes: time.minutes, seconds }
+      );
+    }, 1000);
+    return () => clearInterval(ticker);
+  });
+  return <div>{`${time.minutes}:${time.seconds}`}</div>;
+};
+
+export default TimerDisplay;
